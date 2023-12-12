43328cf81016bd54e477e32e977782dc7cdbc5ba
OQASergey <sergey.qa.63@protonmail.com>
2023-12-12 19:02:57 +0400

    Изменён файл functions.py - добавлен раздел #именованные аргументы

--- functions.py
+++ functions.py
@@ -5,8 +5,22 @@ my_func()
 my_func()
 
 print('===')
-def fa(str):
-    print(str)
-
+def fa(line):
+    print(line)
 fa('hello')
 fa('world')
+fa(str)
+
+print('***')
+def faa(line, abcd, aaa):
+    print(line, abcd, aaa)
+faa('hello', ',', 'world!')
+
+#именованные аргументы
+print('===')
+def fb(line1, line2):
+    print(line1, line2)
+fb(line2='world!', line1='Hello,')
+
+
+
